35ac18f0d31316301c542a300b6b45a23eee6964
Mohamed Naeem <98890695+mohamednaeemm@users.noreply.github.com>
2025-07-15 14:36:46 +0300

    [167] [Time Beats: 35.36%] [Memory Beats: 9.62%] - LeetPush

---
+++ Two-Sum-II---Input-Array-Is-Sorted.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        n = len(numbers)
+
+        left = 0
+        right = n - 1
+        if n < 2:
+            return []
+        
+        
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                right -= 1
+        return []
+
+            
