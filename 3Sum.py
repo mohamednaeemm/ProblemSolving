a14c8cf60765f0a955dfad4c800a97794528297e
Mohamed Naeem <98890695+mohamednaeemm@users.noreply.github.com>
2025-07-19 12:36:44 +0300

    [15] [Time Beats: 96.24%] [Memory Beats: 69.14%] - LeetPush

--- 3Sum.py
+++ 3Sum.py
@@ -1,29 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
         res = []
-        
-        for i in range(len(nums) - 2):  
-            if i > 0 and nums[i] == nums[i - 1]:
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+
+            if i > 0 and a == nums[i - 1]:
                 continue
-                
-            left = i + 1
-            right = len(nums) - 1
-            
-            while left < right:
-                curr_sum = nums[i] + nums[left] + nums[right]
-                
-                if curr_sum == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-                elif curr_sum < 0:
-                    left += 1
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
                 else:
-                    right -= 1
-        
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+                        
         return res
